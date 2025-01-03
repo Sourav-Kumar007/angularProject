5fbc1902509546adb3134282bd9d35a5afa50a9c
Sourav-Kumar007 <souravkumarroy813965@gmail.com>
2025-01-03 16:42:06 +0600

    changes

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component ,Input } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
+  standalone:true,
   imports: [],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
 
+  @Input() course:any;
+ 
+
 }
+// const obj1 = {a:1 , b:2};
+// const obj2 = {b:3 , c:3};
+// const combinedObj = {...obj1...obj2};
+// console.log(combinedObj);
